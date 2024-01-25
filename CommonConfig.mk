39f77dd5dcff7be99646f9640d034eb96ef6f11c
IczYn <iczyn.iczyn@gmail.com>
2024-01-25 07:34:23 +0100

    Rhodep
    Signed-off-by: IczYn <iczyn.iczyn@gmail.com>

--- CommonConfig.mk
+++ CommonConfig.mk
@@ -17,8 +17,6 @@ COMMON_PATH := device/motorola/common
 
 # Fixes
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
-BUILD_BROKEN_DUP_RULES := true
-BUILD_BROKEN_DUP_RULES := true
 
 # Architecture
 TARGET_ARCH := arm64
